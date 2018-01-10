c8cb423de8cf48fe73e2c5dacc92b4c9eb2af01a
Auston Stewart <auston@shedbuilt.net>
2018-01-09 19:01:54 -0500

    Updating to dbus 1.12.2

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@
             --disable-static \
             --disable-doxygen-docs \
             --disable-xml-docs \
-            --docdir=/usr/share/doc/dbus-1.10.22 \
-            --with-console-auth-dir=/run/console
-make -j $SHED_NUMJOBS
-make "DESTDIR=${SHED_FAKEROOT}" install
+            --docdir=/usr/share/doc/dbus-1.12.2 \
+            --with-console-auth-dir=/run/console || exit 1
+make -j $SHED_NUMJOBS || exit 1
+make "DESTDIR=${SHED_FAKEROOT}" install || exit 1
 mkdir -v "${SHED_FAKEROOT}/lib"
 mv -v ${SHED_FAKEROOT}/usr/lib/libdbus-1.so.* "${SHED_FAKEROOT}/lib"
 ln -sfv ../../lib/$(readlink ${SHED_FAKEROOT}/usr/lib/libdbus-1.so) "${SHED_FAKEROOT}/usr/lib/libdbus-1.so"
