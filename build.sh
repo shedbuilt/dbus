a8cacbc4f95428d0e21c1182b6cc1ee4aa87fc02
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:22:55 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -7,10 +7,10 @@
             --disable-xml-docs \
             --docdir=/usr/share/doc/dbus-1.12.6 \
             --with-console-auth-dir=/run/console && \
-make -j $SHED_NUMJOBS && \
-make DESTDIR="$SHED_FAKEROOT" install || exit 1
-mkdir -v "${SHED_FAKEROOT}/lib"
-mv -v "${SHED_FAKEROOT}"/usr/lib/libdbus-1.so.* "${SHED_FAKEROOT}/lib"
-ln -sfv ../../lib/$(readlink "${SHED_FAKEROOT}/usr/lib/libdbus-1.so") "${SHED_FAKEROOT}/usr/lib/libdbus-1.so"
-mkdir -pv "${SHED_FAKEROOT}/var/lib"
-ln -sfv /etc/machine-id "${SHED_FAKEROOT}/var/lib/dbus"
+make -j $SHED_NUM_JOBS && \
+make DESTDIR="$SHED_FAKE_ROOT" install || exit 1
+mkdir -v "${SHED_FAKE_ROOT}/lib"
+mv -v "${SHED_FAKE_ROOT}"/usr/lib/libdbus-1.so.* "${SHED_FAKE_ROOT}/lib"
+ln -sfv ../../lib/$(readlink "${SHED_FAKE_ROOT}/usr/lib/libdbus-1.so") "${SHED_FAKE_ROOT}/usr/lib/libdbus-1.so"
+mkdir -pv "${SHED_FAKE_ROOT}/var/lib"
+ln -sfv /etc/machine-id "${SHED_FAKE_ROOT}/var/lib/dbus"
