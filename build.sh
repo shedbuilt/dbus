1e0c8a34346de615f0887bd2f76a21c368bcd8b9
Auston Stewart <auston@shedbuilt.net>
2018-05-30 17:45:42 -1000

    Updating to v1.12.8 for blank

--- build.sh
+++ build.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
+declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
+SHED_PKG_LOCAL_DOCDIR="/usr/share/doc/${SHED_PKG_NAME}-${SHED_PKG_VERSION}"
+# Configure
 ./configure --prefix=/usr \
             --sysconfdir=/etc \
             --localstatedir=/var \
             --disable-static \
             --disable-doxygen-docs \
             --disable-xml-docs \
-            --docdir=/usr/share/doc/dbus-1.12.6 \
-            --with-console-auth-dir=/run/console && \
-make -j $SHED_NUM_JOBS && \
-make DESTDIR="$SHED_FAKE_ROOT" install || exit 1
-mkdir -v "${SHED_FAKE_ROOT}/lib"
-mv -v "${SHED_FAKE_ROOT}"/usr/lib/libdbus-1.so.* "${SHED_FAKE_ROOT}/lib"
-ln -sfv ../../lib/$(readlink "${SHED_FAKE_ROOT}/usr/lib/libdbus-1.so") "${SHED_FAKE_ROOT}/usr/lib/libdbus-1.so"
-mkdir -pv "${SHED_FAKE_ROOT}/var/lib"
-ln -sfv /etc/machine-id "${SHED_FAKE_ROOT}/var/lib/dbus"
+            --docdir=$SHED_PKG_LOCAL_DOCDIR \
+            --with-console-auth-dir=/run/console &&
+# Build and Install
+make -j $SHED_NUM_JOBS &&
+make DESTDIR="$SHED_FAKE_ROOT" install &&
+# Rearrange
+mkdir -v "${SHED_FAKE_ROOT}/lib" &&
+mv -v "${SHED_FAKE_ROOT}"/usr/lib/libdbus-1.so.* "${SHED_FAKE_ROOT}/lib" &&
+ln -sfv ../../lib/$(readlink "${SHED_FAKE_ROOT}/usr/lib/libdbus-1.so") "${SHED_FAKE_ROOT}/usr/lib/libdbus-1.so" &&
+mkdir -pv "${SHED_FAKE_ROOT}/var/lib" &&
+ln -sfv /etc/machine-id "${SHED_FAKE_ROOT}/var/lib/dbus" || exit 1
+# Optionally Remove Documentation
+if [ -z "${SHED_PKG_LOCAL_OPTIONS[docs]}" ]; then
+    rm -rf "${SHED_FAKE_ROOT}/usr/share/doc"
+fi
